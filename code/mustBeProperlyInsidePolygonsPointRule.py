ab5b8331b94c828644d178a87d7e08b7d62c521e
jolicar <jolivas@gvsig.com>
2020-06-12 11:34:08 +0200

    mod rule files

--- code/mustBeProperlyInsidePolygonsPointRule.py
+++ code/mustBeProperlyInsidePolygonsPointRule.py
@@ -130,8 +130,8 @@ class MustBeProperlyInsidePolygonsPointRule(AbstractTopologyRule):
           nPrimitives = point1.getPrimitivesNumber()
           for i in range(0, nPrimitives-1):
             point=point1.getPointAt(i)
-            point = geomManager.createPoint(point.getX(),point.getY(), subtype)
-            multipoint.addPoint(point)
+            nPoint = geomManager.createPoint(point.getX(),point.getY(), subtype)
+            multipoint.addPoint(nPoint)
         else:
           multipoint=point1
         if not operation(multipoint, dataSet2):
